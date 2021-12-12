2f5c1517644d1bf9ca9657805f0ab7e376b306dc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-12 15:36:44 +0100

    Flare_S8_Pro: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: cherry_mobile Model: Flare_S8_Pro Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_Flare_S8_Pro-user
+add_lunch_combo omni_Flare_S8_Pro-userdebug
+add_lunch_combo omni_Flare_S8_Pro-eng
